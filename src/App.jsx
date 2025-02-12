18bfbfc6fa57ed9e269a3248b54e3b059b0d3e6b
LuisCanabarra <luis7nico@gmail.com>
2025-02-12 10:40:11 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TechQuiz from './components/TechQuiz';
+
+function App() {
+  return (
+    <div className="app">
+      <TechQuiz />
+    </div>
+  );
+}
+
+export default App;
